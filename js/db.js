d9e49e565a60eb538e666d24f5346ecb9dfb4dd8
Marcos Sevilla <marcossevilla14@gmail.com>
2019-05-31 13:31:16 -0600

    real-time data and firestore

---
+++ js/db.js
@@ -0,0 +1,19 @@
+// real-time listener
+db.collection('coffee').onSnapshot((snapshot) => {
+    // console.log(snapshot.docChanges());
+
+    // log out change and data
+    snapshot.docChanges().forEach(change => {
+        // console.log(change, change.doc.data(), change.doc.id);
+
+        if (change.type === 'added') {
+            // add the document data to the web app
+
+        } else if (change.type === 'removed') {
+            // remove the document data from the web app
+
+        }
+    });
+
+
+})
